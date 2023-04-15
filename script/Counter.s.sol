37878911c40e72d562a82640834e2d2f88d9239e
spengrah <code@spengrah.xyz>
2023-04-15 09:21:20 +0900

    fix formatting

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+  function setUp() public { }
 
-    function run() public {
-        uint256 privKey = vm.envUint("PRIVATE_KEY");
-        address deployer = vm.rememberKey(privKey);
-        vm.startBroadcast(deployer);
+  function run() public {
+    uint256 privKey = vm.envUint("PRIVATE_KEY");
+    address deployer = vm.rememberKey(privKey);
+    vm.startBroadcast(deployer);
 
-        vm.stopBroadcast();
-    }
+    vm.stopBroadcast();
+  }
 }
 
 // forge script script/CounterScript.s.sol -f ethereum --broadcast --verify
